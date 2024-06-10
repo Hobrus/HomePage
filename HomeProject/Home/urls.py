7ee27a0af1d303dc03488f548838df16a076a0f4
Hobrus <bhobrus@gmail.com>
2024-06-10 20:28:03 +0300

    6rd lesson

--- HomeProject/Home/urls.py
+++ HomeProject/Home/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('example', views.example, name='example'),
-    path('goods', views.goods_view, name='goods')
+    path('goods', views.goods_view, name='goods'),
+    path('register', views.register_view, name='register'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
 ]
--- HomeProject/Home/views.py
+++ HomeProject/Home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegisterForm
 from .models import Good
 
 
@@ -26,3 +28,42 @@ def goods_view(request):
         "goods": goods_list
     }
     return render(request, 'goods.html', context)
+
+
+def register_view(request):
+    if request.method == 'POST':
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            redirect('home')
+    else:
+        register_form = RegisterForm()
+    context = {
+        'form': register_form
+    }
+    return render(request, 'register.html', context)
+
+
+def login_view(request):
+    if request.method == 'POST':
+        login_form = AuthenticationForm(request, data=request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return redirect('register')
+    else:
+        login_form = AuthenticationForm()
+    context = {
+        'form': login_form
+    }
+    return render(request, 'login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
